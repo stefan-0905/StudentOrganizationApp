d5938ee8511bc30fba5f8ad438912b9264db2e47
Stefan Djordjevic <stefan_0905@hotmail.com>
2019-05-10 13:35:29 +0200

    Added Navigation

---
+++ StudentOrganizationApp/Dashboard.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace StudentOrganizationApp
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.forumButton = new System.Windows.Forms.Button();
+            this.messagesButton = new System.Windows.Forms.Button();
+            this.coursesButton = new System.Windows.Forms.Button();
+            this.announcementsButton = new System.Windows.Forms.Button();
+            this.dashboardButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gold;
+            this.panel1.Controls.Add(this.forumButton);
+            this.panel1.Controls.Add(this.messagesButton);
+            this.panel1.Controls.Add(this.coursesButton);
+            this.panel1.Controls.Add(this.announcementsButton);
+            this.panel1.Controls.Add(this.dashboardButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 540);
+            this.panel1.TabIndex = 0;
+            // 
+            // forumButton
+            // 
+            this.forumButton.FlatAppearance.BorderSize = 0;
+            this.forumButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(106)))), ((int)(((byte)(134)))));
+            this.forumButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.forumButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.forumButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(100)))), ((int)(((byte)(183)))));
+            this.forumButton.Location = new System.Drawing.Point(0, 326);
+            this.forumButton.Name = "forumButton";
+            this.forumButton.Size = new System.Drawing.Size(200, 49);
+            this.forumButton.TabIndex = 4;
+            this.forumButton.Text = "Forum";
+            this.forumButton.UseVisualStyleBackColor = true;
+            this.forumButton.MouseEnter += new System.EventHandler(this.NavigationButton_MouseEnter);
+            this.forumButton.MouseLeave += new System.EventHandler(this.NavigationButton_MouseLeave);
+            // 
+            // messagesButton
+            // 
+            this.messagesButton.FlatAppearance.BorderSize = 0;
+            this.messagesButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(106)))), ((int)(((byte)(134)))));
+            this.messagesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.messagesButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.messagesButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(100)))), ((int)(((byte)(183)))));
+            this.messagesButton.Location = new System.Drawing.Point(0, 277);
+            this.messagesButton.Name = "messagesButton";
+            this.messagesButton.Size = new System.Drawing.Size(200, 49);
+            this.messagesButton.TabIndex = 3;
+            this.messagesButton.Text = "Messages";
+            this.messagesButton.UseVisualStyleBackColor = true;
+            this.messagesButton.MouseEnter += new System.EventHandler(this.NavigationButton_MouseEnter);
+            this.messagesButton.MouseLeave += new System.EventHandler(this.NavigationButton_MouseLeave);
+            // 
+            // coursesButton
+            // 
+            this.coursesButton.FlatAppearance.BorderSize = 0;
+            this.coursesButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(106)))), ((int)(((byte)(134)))));
+            this.coursesButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.coursesButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.coursesButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(100)))), ((int)(((byte)(183)))));
+            this.coursesButton.Location = new System.Drawing.Point(0, 228);
+            this.coursesButton.Name = "coursesButton";
+            this.coursesButton.Size = new System.Drawing.Size(200, 49);
+            this.coursesButton.TabIndex = 2;
+            this.coursesButton.Text = "Courses";
+            this.coursesButton.UseVisualStyleBackColor = true;
+            this.coursesButton.MouseEnter += new System.EventHandler(this.NavigationButton_MouseEnter);
+            this.coursesButton.MouseLeave += new System.EventHandler(this.NavigationButton_MouseLeave);
+            // 
+            // announcementsButton
+            // 
+            this.announcementsButton.FlatAppearance.BorderSize = 0;
+            this.announcementsButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(106)))), ((int)(((byte)(134)))));
+            this.announcementsButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.announcementsButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.announcementsButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(100)))), ((int)(((byte)(183)))));
+            this.announcementsButton.Location = new System.Drawing.Point(0, 179);
+            this.announcementsButton.Name = "announcementsButton";
+            this.announcementsButton.Size = new System.Drawing.Size(200, 49);
+            this.announcementsButton.TabIndex = 1;
+            this.announcementsButton.Text = "Announcements";
+            this.announcementsButton.UseVisualStyleBackColor = true;
+            this.announcementsButton.MouseEnter += new System.EventHandler(this.NavigationButton_MouseEnter);
+            this.announcementsButton.MouseLeave += new System.EventHandler(this.NavigationButton_MouseLeave);
+            // 
+            // dashboardButton
+            // 
+            this.dashboardButton.FlatAppearance.BorderSize = 0;
+            this.dashboardButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(101)))), ((int)(((byte)(106)))), ((int)(((byte)(134)))));
+            this.dashboardButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dashboardButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboardButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(100)))), ((int)(((byte)(183)))));
+            this.dashboardButton.Location = new System.Drawing.Point(0, 130);
+            this.dashboardButton.Name = "dashboardButton";
+            this.dashboardButton.Size = new System.Drawing.Size(200, 49);
+            this.dashboardButton.TabIndex = 0;
+            this.dashboardButton.Text = "Dashboard";
+            this.dashboardButton.UseVisualStyleBackColor = true;
+            this.dashboardButton.MouseEnter += new System.EventHandler(this.NavigationButton_MouseEnter);
+            this.dashboardButton.MouseLeave += new System.EventHandler(this.NavigationButton_MouseLeave);
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 540);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Dashboard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Student Organization";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button dashboardButton;
+        private System.Windows.Forms.Button forumButton;
+        private System.Windows.Forms.Button messagesButton;
+        private System.Windows.Forms.Button coursesButton;
+        private System.Windows.Forms.Button announcementsButton;
+    }
+}
---
+++ StudentOrganizationApp/Dashboard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentOrganizationApp
+{
+    public partial class Dashboard : Form
+    {
+        List<Button> navigationButtons = new List<Button>();
+        public Dashboard()
+        {
+            InitializeComponent();
+            navigationButtons.Add(dashboardButton);
+            navigationButtons.Add(announcementsButton);
+            navigationButtons.Add(coursesButton);
+            navigationButtons.Add(messagesButton);
+            navigationButtons.Add(forumButton);
+        }
+
+        private void NavigationButton_MouseEnter(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.ForeColor = Color.Gold;
+        }
+
+        private void NavigationButton_MouseLeave(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            button.ForeColor = Color.FromArgb(47, 100, 183);
+        }
+    }
+}
