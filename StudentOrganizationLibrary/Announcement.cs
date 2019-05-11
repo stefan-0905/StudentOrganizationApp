f0e82fe554f7a423102d16f930033be79065447a
Stefan Djordjevic <stefan_0905@hotmail.com>
2019-05-11 21:30:02 +0200

    Changed wrong class name from Post to Announcement

---
+++ StudentOrganizationLibrary/Announcement.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentOrganizationLibrary
+{
+    public class Announcement
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public Announcement(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
+    }
+}
