64956ce0d795febb41e07ac6702907f57433b298
Stefan Djordjevic <stefan_0905@hotmail.com>
2019-05-11 21:26:12 +0200

    Created UserControl for displaying announcement

---
+++ StudentOrganizationControlLibrary/CardControl.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace StudentOrganizationControlLibrary
+{
+    partial class CardControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Card = new System.Windows.Forms.Panel();
+            this.headerLabel = new System.Windows.Forms.Label();
+            this.descriptionLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Card.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Card
+            // 
+            this.Card.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(47)))), ((int)(((byte)(100)))), ((int)(((byte)(183)))));
+            this.Card.Controls.Add(this.button1);
+            this.Card.Controls.Add(this.headerLabel);
+            this.Card.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Card.Location = new System.Drawing.Point(0, 0);
+            this.Card.Name = "Card";
+            this.Card.Size = new System.Drawing.Size(300, 18);
+            this.Card.TabIndex = 0;
+            // 
+            // headerLabel
+            // 
+            this.headerLabel.AutoSize = true;
+            this.headerLabel.ForeColor = System.Drawing.Color.White;
+            this.headerLabel.Location = new System.Drawing.Point(3, 2);
+            this.headerLabel.Margin = new System.Windows.Forms.Padding(0);
+            this.headerLabel.Name = "headerLabel";
+            this.headerLabel.Size = new System.Drawing.Size(122, 13);
+            this.headerLabel.TabIndex = 1;
+            this.headerLabel.Text = "Announcement Heading";
+            // 
+            // descriptionLabel
+            // 
+            this.descriptionLabel.AutoSize = true;
+            this.descriptionLabel.Location = new System.Drawing.Point(2, 20);
+            this.descriptionLabel.Name = "descriptionLabel";
+            this.descriptionLabel.Size = new System.Drawing.Size(174, 13);
+            this.descriptionLabel.TabIndex = 1;
+            this.descriptionLabel.Text = "Some desctiption of announcement";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(221, 1);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 15);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Read more";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // CardControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.descriptionLabel);
+            this.Controls.Add(this.Card);
+            this.Margin = new System.Windows.Forms.Padding(0, 0, 0, 3);
+            this.Name = "CardControl";
+            this.Size = new System.Drawing.Size(300, 36);
+            this.Card.ResumeLayout(false);
+            this.Card.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Card;
+        public System.Windows.Forms.Label descriptionLabel;
+        public System.Windows.Forms.Label headerLabel;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ StudentOrganizationControlLibrary/CardControl.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentOrganizationControlLibrary
+{
+    public partial class CardControl : UserControl
+    {
+        public CardControl()
+        {
+            InitializeComponent();
+            
+        }
+        public CardControl(string heading, string description)
+        {
+            InitializeComponent();
+
+            Graphics g = CreateGraphics();
+            SizeF size = g.MeasureString(heading, headerLabel.Font);
+            
+            if (size.Width > 115)
+            {
+                heading = heading.Substring(0, 35);
+                heading = string.Concat(heading, "...");
+            }
+            headerLabel.Text = heading;
+
+            size = g.MeasureString(description, descriptionLabel.Font);
+
+            if (size.Width > 294)
+            {
+                MessageBox.Show(size.ToString());
+                description = description.Substring(0, 50);
+                description = string.Concat(description, "...");
+            }
+            descriptionLabel.Text = description;
+        }
+    }
+}
