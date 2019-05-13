89967bd029f01071cbcef65adac3485e71cac6e2
Stefan Djordjevic <stefan_0905@hotmail.com>
2019-05-13 20:09:14 +0200

    Updated class with new CreatedAt field

--- StudentOrganizationLibrary/Announcement.cs
+++ StudentOrganizationLibrary/Announcement.cs
@@ -11,11 +11,16 @@ namespace StudentOrganizationLibrary
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public Announcement() { }
+        public DateTime CreatedAt { get; set; }
+        public Announcement()
+        {
+            CreatedAt = DateTime.Now;
+        }
         public Announcement(string title, string description)
         {
             Title = title;
             Description = description;
+            CreatedAt = DateTime.Now;
         }
     }
 }
